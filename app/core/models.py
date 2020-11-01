9ce1b139c1bbb288af12005b3470d3ca50eae46b
marciodasilva <marciodasilva@hotmail.com>
2020-11-01 12:49:01 -0800

    Created endpoint for creating users

--- app/core/models.py
+++ app/core/models.py
@@ -4,31 +4,34 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 
 
 class UserManager(BaseUserManager):
+
     def create_user(self, email, password=None, **extra_fields):
-        """ Create and Save a new user """
+        """Creates and saves a new user"""
         if not email:
-            raise ValueError("Users must have email address")
+            raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
     def create_superuser(self, email, password):
-        """ Create and save a superuser """
+        """Creates and saves a new super user"""
         user = self.create_user(email, password)
-        user.is_superuser = True
         user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """ Customer User Model that support email """
+    """Custom user model that suppors using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
+
     USERNAME_FIELD = 'email'
